d7a57b62448a725995647753c57191984beeec21
EmanueleVenditti95 <vendittiema@gmail.com>
2024-01-17 16:38:00 +0100

    Add - api for TypeShow

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
@@ -15,5 +16,15 @@ class TypeController extends Controller
             'success' => true,
             'results' => $types
         ]);
+    }
+
+    public function show(Type $type) {
+        
+        $projects = Project::with('technologies')->where('type_id',$type->id)->orderBy('updated_at', 'desc')->paginate(10);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);        
     }
 }
