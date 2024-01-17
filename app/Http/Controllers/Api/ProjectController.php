8c5a685a47dcad59bddbaf15b3bc3e47a591e21b
EmanueleVenditti95 <vendittiema@gmail.com>
2024-01-17 12:51:43 +0100

    Add - route,api for TypesIndex

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,6 +17,7 @@ class ProjectController extends Controller
     }
 
     public function show(Project $project) {
+        
         $project->load('type','technologies');
 
         return response()->json([
